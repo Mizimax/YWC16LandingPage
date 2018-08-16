f880fa94e236fd8cbf9bdbe0a2b4c4364277f29a
Max ang <maxsayr5@gmail.com>
2018-08-17 00:09:29 +0700

    js coming soon ...

---
+++ src/js/comingsoon.js
@@ -0,0 +1,12 @@
+var isComing = function() {
+  var text = $(".js--comingsoon__text").text() + ".";
+  $(".js--comingsoon__text").text(text);
+
+  if ($(".js--comingsoon__text").text() === "....") {
+    $(".js--comingsoon__text").text("");
+  }
+};
+
+$(document).ready(function() {
+  setInterval(isComing, 1000);
+});
