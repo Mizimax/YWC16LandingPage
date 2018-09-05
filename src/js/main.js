1131926811559036d4bb85098caae91fe3737413
Max ang <maxsayr5@gmail.com>
2018-09-06 02:46:34 +0700

    fix guru interval bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,7 @@
+var guruInterval;
+
 $(document).ready(function() {
-  // Guru change
-  var next = $(".guru__child:first").next(":first");
-  setInterval(function() {
-    var pic = next.children(":first").attr("src");
-    var name = next.children(":first").attr("name");
-    var corp = next.children(":first").attr("corp");
-
-    $(".guru__child.active").removeClass("active");
-    next.addClass("active");
-
-    $(".js--guru_fade").fadeOut(200, function() {
-      $(".guru__main__pic__img").attr("src", pic);
-      $(".guru__main__info__name").text(name);
-      $(".guru__main__info__corp").text(corp);
-      $(".js--guru_fade").fadeIn(200);
-    });
-    next = next.next(":first");
-    if (next.length === 0) {
-      next = $(".guru__child:first");
-    }
-  }, 5000);
-
-  function showHideQuestion(parentElem){
+  function showHideQuestion(parentElem) {
     if (parentElem.hasClass("active")) {
       parentElem.removeClass("active");
     } else {
@@ -36,9 +16,35 @@ $(document).ready(function() {
   });
   $(".faq__question").click(function(e) {
     e.preventDefault();
-    if(!$(this).hasClass("active"))
-      showHideQuestion($(this));
+    if (!$(this).hasClass("active")) showHideQuestion($(this));
   });
+
+  // Guru change
+  var nextEle = $(".guru__child:first").next(":first");
+  var setGuruInterval = function(next) {
+    var guruInterval = setInterval(function() {
+      var pic = next.children(":first").attr("src");
+      var name = next.children(":first").attr("name");
+      var corp = next.children(":first").attr("corp");
+
+      $(".guru__child.active").removeClass("active");
+      next.addClass("active");
+
+      $(".js--guru_fade").fadeOut(200, function() {
+        $(".guru__main__pic__img").attr("src", pic);
+        $(".guru__main__info__name").text(name);
+        $(".guru__main__info__corp").text(corp);
+        $(".js--guru_fade").fadeIn(200);
+      });
+      next = next.next(":first");
+      if (next.length === 0) {
+        next = $(".guru__child:first");
+      }
+    }, 5000);
+    return guruInterval;
+  };
+
+  guruInterval = setGuruInterval(nextEle);
 
   $(".guru__child").click(function() {
     var pic = $(this)
@@ -60,6 +66,10 @@ $(document).ready(function() {
       $(".guru__main__info__corp").text(corp);
       $(".js--guru_fade").fadeIn(200);
     });
+
+    clearInterval(guruInterval);
+    var nextEle = $(this).next(":first");
+    guruInterval = setGuruInterval(nextEle);
   });
   var carousels = bulmaCarousel.attach();
 });
